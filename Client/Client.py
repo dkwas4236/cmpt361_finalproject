f7b9c720e348b9bd5f31c1ce5f2abb61f4eb1f3a
LPmemoh <163183627+LPmemoh@users.noreply.github.com>
2024-03-25 23:19:08 -0600

    Update Client.py

--- Client/Client.py
+++ Client/Client.py
@@ -1,13 +1,17 @@
 # Client.py
 import socket, sys, os, glob, datetime, json
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad,unpad
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.Util.Padding import pad, unpad
 from Crypto.PublicKey import RSA
 
 def client():
     serverPort = 13000
 
-    # Create cloent socket using IPv4 and TCP protocols
+    # retrieve server public keys
+    with open('server_public.pem', 'rb') as file:
+        server_public_key = RSA.import_key(file.read())
+
+    # Create client socket using IPv4 and TCP protocols
     try:
         clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error as e:
@@ -17,21 +21,36 @@ def client():
     try:
         # Get IP or Server name from user
         serverName = input('Enter the server IP or name: ') 
-        # Client conncets with the server
+        # Client connects with the server
         clientSocket.connect((serverName, serverPort))
 
         # Client receives the initial username message
         usernameReq = clientSocket.recv(2048).decode('ascii') 
         username = input(usernameReq)
-        # Send username back to server 
-        clientSocket.send(username.encode('ascii'))
+        # Send username back to server
+        username_encrypted = rsa_encrypt(username, server_public_key)
+        clientSocket.send(username_encrypted)
         # Receive password message 
         passwordReq = clientSocket.recv(2048).decode('ascii') 
         password = input(passwordReq)
         # send password back to server
-        clientSocket.send(password.encode('ascii'))
+        password_encrypted = rsa_encrypt(password, server_public_key)
+        clientSocket.send(password_encrypted)
 
-        # Recive menu from server
+        # grab the users private key by taking their username
+        with open('%s_private.pem' % username, 'rb') as file:
+            client_private_key = RSA.import_key(file.read())
+        # receive length of encrypted key
+        encrypted_length = int.from_bytes(clientSocket.recv(4), 'big')
+        # receive the encrypted key based on the length
+        symkey_encrypted = b''
+        while len(symkey_encrypted) < encrypted_length:
+            part = clientSocket.recv(encrypted_length - len(symkey_encrypted))
+            symkey_encrypted += part
+        sym_key = rsa_decrypt(symkey_encrypted, client_private_key)
+        encrypt("OK", sym_key, clientSocket)
+
+        # Receive menu from server
         menu = clientSocket.recv(2048).decode('ascii')
         if not menu.endswith(": "):
             print(menu)
@@ -40,7 +59,7 @@ def client():
          
         # Loop until user termination
         while(1):
-            # Display menu and get user chice 
+            # Display menu and get user choice
             choice = input(menu)
             # Send choice to server
             clientSocket.send(choice.encode('ascii'))
@@ -93,6 +112,30 @@ def client():
     except socket.error as e:
         print('An error occurred:', e)
         sys.exit(1)
+
+# RSA decrypt method for initial handshake between client and server
+def rsa_decrypt(encrypted_message, private_key):
+    cipher = PKCS1_OAEP.new(private_key)
+    decrypted_message = cipher.decrypt(encrypted_message)
+    return decrypted_message
+
+# RSA encrypt method for initial handshake between client and server
+def rsa_encrypt(message, public_key):
+    cipher = PKCS1_OAEP.new(public_key)
+    encrypted_message = cipher.encrypt(message.encode())
+    return encrypted_message
+
+# encrypts messages sent by client using AES (symmetric keys)
+def encrypt(message, key, socket):
+    cipher = AES.new(key, AES.MODE_ECB)
+    cipher_bytes = cipher.encrypt(pad(message.encode(), AES.block_size))
+    socket.send(cipher_bytes)
+
+# decrypts messages sent by serve using AES (symmetric keys)
+def decrypt(ciphertext, key):
+    cipher = AES.new(key, AES.MODE_ECB)
+    plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
+    return plaintext.decode()
 
 # call the client function below
 if __name__ == "__main__":
